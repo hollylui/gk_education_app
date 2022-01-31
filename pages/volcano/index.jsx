09d4ff8c536b91bc205ec19ad95481807007d67f
hollylui <hollylui@gmail.com>
2022-01-31 15:00:06 +0100

    age component is done

--- pages/volcano/index.jsx
+++ pages/volcano/index.jsx
@@ -1,10 +1,5 @@
-import Age from "../../components/Age";
 import styles from "../../styles/game_example.module.scss";
 
 export default function Game() {
-  return (
-    <div>
-      <Age />
-    </div>
-  );
+  return <div></div>;
 }
