8c9f2a1f650d939b2549f87c2d5c57c353c4d5a5
Bella Yang <bellalucajosef@gmail.com>
2022-02-15 10:08:48 +0100

    saved

--- components/Quiz.jsx
+++ components/Quiz.jsx
@@ -1,32 +1,28 @@
 import { useState, useContext, useEffect } from "react";
+import { useRouter } from "next/router";
 import BackpackContext from "../context/BackpackContext";
 import ReactModal from "react-modal";
+
+import GameContext from "../context/GameContext";
 
 import styles from "../styles/Quiz.module.scss";
 
 export default function Quiz({ questions, item }) {
+  const router = useRouter();
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [isCorrect, setIsCorrect] = useState(false); //user items is an empty array in default state
   const { userItems, setUserItems } = useContext(BackpackContext);
 
+  const { currGameId, gameIds } = useContext(GameContext);
+  const nextGameId = gameIds[gameIds.indexOf(currGameId) + 1];
+
   let items = ["coconut", "fire", "leaf", "stone", "water"];
 
-  //*index for choosing item from 'items' array.
-  const [index, setIndex] = useState(0);
-
-  //*add item to userItems state (why ternary operator?: to prevent showing 'null' when index is 0)
-  function addItem(_index) {
-    // let backpackContentsHowMany = userItems.length();
-    // let item = items[backpackContentsHowMany];
-    index === 0
-      ? setUserItems([])
-      : setUserItems([...userItems, items[_index - 1]]);
+  function addItem() {
+    let nextItemIndex = userItems.length;
+    let item = items[nextItemIndex];
+    setUserItems([...userItems, item]);
   }
-
-  //* there are 2 addItem which have same name. so I commented in.
-  // function addItem() {
-  //   setUserItems(userItems.push(item));
-  // }
 
   function makeCorrect() {
     setIsCorrect(true);
@@ -36,44 +32,38 @@ export default function Quiz({ questions, item }) {
   }
 
   function closeModal() {
-    setModalIsOpen(false);
+    router.push(`/volcano/${nextGameId}`);
   }
 
   const handleClick = async (event) => {
     let value = event.target.value;
 
-    console.log(typeof value);
-
     if (value === "true") {
-      // addItem();
       makeCorrect();
-      console.log("setting to correct");
-      //* If the user answers correctly, then the index is incremented.
-      setIndex(index + 1);
+      addItem();
     }
-
     openModal();
   };
 
-  useEffect(() => {
-    //* Whenever the index changes, using this index as an index number for choosing a specific item in 'items' array, and then add the item to userItems state.
-    if (index >= 0 && index <= items.length) {
-      addItem(index);
-    }
-  }, [index]);
+  // useEffect(() => {
+  //   //* Whenever the index changes, using this index as an index number for choosing a specific item in 'items' array, and then add the item to userItems state.
+  //   // if (index >= 0 && index <= items.length) {
+  //   //   addItem(index);
+  //   // }
+  // }, [index]);
 
   //* when this component mounts, get items from localstorage,
   //* If there are items inside localstorage, save them to userItems state.
   useEffect(() => {
-    const gotItems = window.localStorage.getItem("useritems");
-    if (gotItems !== null) {
-      setUserItems(JSON.parse(window.localStorage.getItem("useritems")));
-    }
+    // const gotItems = window.localStorage.getItem("useritems");
+    // if (gotItems !== null) {
+    //   setUserItems(JSON.parse(window.localStorage.getItem("useritems")));
+    // }
   }, []);
 
   //* Whenever userItems changes save it to the localstorage.
   useEffect(() => {
-    localStorage.setItem("useritems", JSON.stringify(userItems));
+    // localStorage.setItem("useritems", JSON.stringify(userItems));
   }, [userItems]);
 
   //pull random question from database
@@ -93,7 +83,7 @@ export default function Quiz({ questions, item }) {
             type="submit"
             value={question.answers[0].correctAnswer}
           >
-            {question.answers[0].text}{" "}
+            {question.answers[0].text}
           </button>
           <button
             className={styles.button}
