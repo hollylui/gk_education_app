0ca208938cc064388eaaa78945da31fe2723e140
JoshZed <jshzed@gmail.com>
2022-02-22 16:20:29 +0100

    special ending

---
+++ components/Special.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import Lottie from "react-lottie";
+import special from "../lotties/special.js";
+import { useRouter } from "next/router";
+import Styles from "./../styles/animation_styles/cartoon.module.scss";
+import Image from "next/image";
+import TitleImage from "../public/specialEnding";
+
+//animation Controlls
+
+const Special = ({ Position }) => {
+  //useRouter to set a timeout for the animation
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/ending/0");
+    }, 5000);
+  }, []);
+
+  //end of useRouter
+
+  const defaultOptions = {
+    loop: false,
+    autoplay: true,
+    animationData: special[Position],
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  return (
+    <div className={`${Styles.controlled} ${Styles.hold}`}>
+      <Image src={TitleImage[Position]} layout="fill" alt="main" />
+      <div className={Styles.lot}>
+        <Lottie options={defaultOptions} height={540} width={960} />
+      </div>
+    </div>
+  );
+};
+
+Special.getInitialProps = ({ query: { id } }) => {
+  const animation = special[id];
+  // const { data } = await axios.get(`http://localhost:3000/api/notes/${id}`);
+  return { animation };
+};
+
+export default Special;
---
+++ public/specialEnding.js
@@ -0,0 +1,19 @@
+import image1 from "../public/images/animations/sky.png";
+import image2 from "../public/images/animations/zoom_in_palms.png";
+import image3 from "../public/images/animations/insidecave.png";
+import image4 from "../public/images/animations/bridge_scorpion_encounter.png";
+import image5 from "../public/images/animations/boat_background.png";
+import image6 from "../public/images/animations/volcano island.png";
+import image7 from "../public/images/animations/volcano_end_image.png";
+import image8 from "../public/images/animations/alien_landing_background.png"
+import image9 from "../public/images/animations/zoom_crash_background.png"
+import image10 from "../public/images/animations/alien_crash_background.png"
+import image11 from "../public/images/animations/alien_crash_background.png"
+import image12 from "../public/images/animations/stars.jpg"
+import image13 from "../public/images/animations/endtitles.png";
+
+// import image9 from "../public/images/animations/alien_landing.png";
+
+const specialEnding = [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12, image13];
+
+export default specialEnding;
