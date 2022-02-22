1358a5d4ab7b376f6122ce24a0abfeb3d8c33b18
hollylui <hollylui@gmail.com>
2022-02-22 13:57:14 +0100

    merge animation

--- next.config.js
+++ next.config.js
@@ -8,9 +8,6 @@ module.exports = {
     webpack5: true, // by default, if you customize webpack config, they switch back to version 4.
     // Looks like backward compatibility approach.
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 
   webpack(config, options) {
     const { isServer } = options;
