8047e95faaf9250b5f92fd5e4679738f47d5f195
JoshZed <jshzed@gmail.com>
2022-02-23 11:21:52 +0100

    special ending

--- pages/volcano/ending/special/[id].js
+++ pages/volcano/ending/special/[id].js
@@ -1,7 +1,48 @@
-import React from "react";
+// import timeout from "../timeout";
+import animationArray from "../../../../lotties/special";
+import { useRouter } from "next/router";
+// import Special from "../../Components/Special";
+import Lottie from "react-lottie";
+import Image from "next/image";
+import { useEffect } from "react";
+import Styles from "./../../../../styles/animation_styles/cartoon.module.scss";
+import TitleImage from "./../../../../public/specialEnding.js";
 
-function specialEndingSequence() {
-  return <div>[id]</div>;
-}
 
-export default specialEndingSequence;
+const SpecialEndingSequence = ({ animation, id }) => {
+  const router = useRouter();
+
+  const defaultOptions = {
+    loop: false,
+    autoplay: true,
+    animationData: animation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  useEffect(() => {
+    if (+id >= 0 && +id < animationArray.length - 1)
+      setTimeout(() => {
+        router.push(`/volcano/ending/special/${+id + 1}`);
+      }, 5000);
+  }, [id]);
+
+  return (
+   
+    <div  className={`${Styles.controlled} ${Styles.hold}`}>
+      <Image src={TitleImage[id]} layout="fill" alt="main" />
+      <div  className={Styles.lot}>
+        <Lottie options={defaultOptions} height={540} width={960} />
+      </div>
+    </div>
+    
+  );
+};
+
+SpecialEndingSequence.getInitialProps = ({ query: { id } }) => {
+  const animation = animationArray[id];
+  return { animation, id };
+};
+
+export default SpecialEndingSequence;
--- pages/volcano/ending/special/index.js
+++ pages/volcano/ending/special/index.js
@@ -1,13 +1,13 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-import styles from "../../../../styles/Special.module.scss";
+
 
 function Special() {
   const router = useRouter();
   useEffect(() => {
     router.push("/volcano/ending/special/0");
   }, []);
-  return <div></div>;
+  return <></>;
 }
 
 export default Special;
